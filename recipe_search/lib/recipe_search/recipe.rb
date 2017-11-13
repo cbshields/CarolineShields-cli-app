90ff89b518094b19ebae0eda072134bc140a7e90
Caroline Shields <caroline.shields@amkor.com>
2017-11-13 22:53:42 +0000

    added preptime and directions

--- recipe_search/lib/recipe_search/recipe.rb
+++ recipe_search/lib/recipe_search/recipe.rb
@@ -1,11 +1,19 @@
 class RecipeSearch::Recipe
 
-attr_accessor :name, :ingredients, :steps, :url
+attr_accessor :name, :ingredients, :steps, :url, :prep
 
 def self.scrape_recipe(url)
   doc = Nokogiri::HTML(open(url))
   ingredient_list = []
   list_steps = []
+  prep_time = []
+
+  #prep times
+  doc.search(".td-recipe-info").each do |preptime|
+    prep_time << preptime.text
+  end
+  binding.pry
+
 
   #getting ingredients
   doc.search("ul.td-arrow-list li").each do |ingred|
@@ -16,21 +24,22 @@ def self.scrape_recipe(url)
   #getting directions span.dropcap
   doc.search(".td-post-content p").collect do |p|
     text = p.text
-    binding.pry
-    if text != "Ingredients:" || text != " "
-      binding.pry
+    #how to remove &nbsp from array  - || text != "\u00A0"
+    if text != "Ingredients:"
       until text[0].to_i == 0
           text = text[1..-1]
       end
       list_steps << text
     end
   end
+  #remove &nbsp
+  list_steps.shift
 
-  binding.pry
   recipe = self.new
+  recipe.name = doc.search("h1.entry-title").text
+  recipe.prep = prep_time.join(" ")
   recipe.ingredients = ingredient_list
   recipe.steps = list_steps
-  recipe.name = doc.search("h1.entry-title").text
   recipe.url = url
   recipe
 end
