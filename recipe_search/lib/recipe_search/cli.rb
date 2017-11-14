61d35b79b81c8b6aa12ee20f00587f484dc9b129
Caroline Shields <caroline.shields@amkor.com>
2017-11-14 00:20:10 +0000

    write code to exit out of program completely

--- recipe_search/lib/recipe_search/cli.rb
+++ recipe_search/lib/recipe_search/cli.rb
@@ -24,27 +24,27 @@ class RecipeSearch::CLI
       if input.to_i > 0
         the_cat = @categories[input.to_i-1]
         the_cat_url = the_cat[:cat_url]
-        puts "#{the_cat[:name]}"
         @recipe_list = RecipeSearch::Scraper.recipe_cat(the_cat_url)
-        list_recipes
-        recipe_choices
+          if @recipe_list.empty?
+            puts "There are no recipes for this category."
+            list_categories
+          else
+            puts "#{the_cat[:name]}"
+            list_recipes
+            recipe_choices
+          end
       elsif input == "list"
         list_categories
       else
         puts "Please enter the number of the recipe category you wish to see"
       end
-    end
-  end
+    end #while loop
+  end #ends menu_categories
 
   def list_recipes
-    if !@recipe_list.empty?
         @recipe_list.each.with_index(1) do |list, i|
           puts "#{i}. #{list[:recipe_name]}"
         end
-    else
-      puts "There are no recipes for this category. Please select another category:"
-      list_categories
-    end
   end
 
   def recipe_choices
@@ -53,32 +53,41 @@ class RecipeSearch::CLI
     while input != "exit"
       #figure out code to exit completely out of program
       #puts "Enter the number of the recipe would you like to see, 'exit' to quit program, 'back' to see the recipe categories or 'list' to view categories again:"
-      puts "Enter the number of the recipe would you like to see, 'exit' to see the recipe categories or 'list' to view categories again:"
+      puts "Enter the number of the recipe would you like to see, 'back' to see the recipe categories,'list' to view categories again or 'exit':"
       input = gets.strip.downcase
       if input.to_i > 0
-        #Brownie Dessert Recipe-ingredients & directions
         the_recipe_info = @recipe_list[input.to_i-1]
         the_recipe_info_url = the_recipe_info[:recipe_url]
         recipe_info(the_recipe_info_url)
       elsif input == "list"
         list_recipes
-      elsif input == "exit"
-        #comment out list_categories when figure out exit code
-        end_recipe_search
-        #list_categories
       elsif input == "back"
         list_categories
       else
-        puts "Please enter the number of the recipe category you wish to see"
+        puts "Please enter the number of the recipe you wish to see"
       end
     end
   end
 
-def recipe_info(recipe_info_url)
-  recipe = RecipeSearch::Recipe.scrape_recipe(recipe_info_url)
-  binding.pry
-end
-
+  def recipe_info(recipe_info_url)
+    recipe = RecipeSearch::Recipe.scrape_recipe(recipe_info_url)
+    puts " "
+    puts "Name: #{recipe.name}"
+    puts "#{recipe.prep}"
+    puts "Ingredients:"
+    puts "==================================================="
+      recipe.ingredients.each.with_index(1) do |list|
+        puts "- #{list}"
+      end
+   puts " "
+   puts "Directions:"
+   puts "==================================================="
+     recipe.steps.each.with_index(1) do |list, i|
+       puts "#{i}. #{list}"
+     end
+   puts " "
+   puts " "
+  end
 
   def end_recipe_search
     puts "Thank you for using the recipe search"
